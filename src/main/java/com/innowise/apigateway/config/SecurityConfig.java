260569d3229cb377679e8c43167da5567cf02a9d
AbsoluteInDoubt <pervysbor@gmail.com>
2026-06-23 17:37:17 +0300

    fix: fixed package structure

--- src/main/java/com/innowise/apigateway/config/SecurityConfig.java
+++ src/main/java/com/innowise/apigateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.innowise.apigateway.security.config;
+package com.innowise.apigateway.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/innowise/apigateway/config/SecurityProperties.java
+++ src/main/java/com/innowise/apigateway/config/SecurityProperties.java
@@ -1,4 +1,4 @@
-package com.innowise.apigateway.security.config;
+package com.innowise.apigateway.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
