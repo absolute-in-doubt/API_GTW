0852a5a20efd01b940b1e6b3fca821af712bc990
AbsoluteInDoubt <pervysbor@gmail.com>
2026-06-23 18:08:23 +0300

    fix(test): added a ReactiveJwtDecoder mock, so the test context loadss fine

--- src/test/java/com/innowise/apigateway/ApigatewayApplicationTests.java
+++ src/test/java/com/innowise/apigateway/ApigatewayApplicationTests.java
@@ -2,11 +2,16 @@ package com.innowise.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApigatewayApplicationTests {
+
+	@MockitoBean
+	private ReactiveJwtDecoder reactiveJwtDecoder;
 
 	@Test
 	void contextLoads() {
